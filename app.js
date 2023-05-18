8e8ad456378d990612e62e7e594ccfa4fa96ff40
brknt <brknt.gns@hotmail.com>
2023-05-18 16:40:55 +0300

    added public and views directory

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const express = require('express');
 const app = express();
 
 
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARE
+app.set(express.static('public'));
+// app.set(express.urlencoded({extended: true})); // urldeki datayı oku
+// app.set(express.json()); // urldeki datayı json formatta oku
+
+
+
 app.get('/',(req, res) =>{
-    res.status(200).send('<h1>INDEX SAYFASI</h1>');
+    res.status(200).render('index'); 
 });
 
 const port = 3000;
