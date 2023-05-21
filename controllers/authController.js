992a8e25f49f87f123703957331ffde45c60388d
brknt <brknt.gns@hotmail.com>
2023-05-22 01:40:33 +0300

    added dashboard page

--- controllers/authController.js
+++ controllers/authController.js
@@ -23,13 +23,14 @@ const loginUser = async (req, res) => {
     const user = await User.findOne({ email: email });
 
     if (user) {
-      bcrypt.compare(password, user.password, (err, same) => {console.log(same);
-      
+      bcrypt.compare(password, user.password, (err, same) => {
+        console.log(same);
+
         if (same) {
           // USER SESSION
           // hangi userın giriş işlemi yaptığını bulalım:
           req.session.userID = user._id;
-          res.status(200).redirect('/');
+          res.status(200).redirect('/users/dashboard');
         }
       });
     }
@@ -41,14 +42,25 @@ const loginUser = async (req, res) => {
   }
 };
 
-const logoutUser = (req, res) =>{
-  req.session.destroy(()=>{
+const logoutUser = (req, res) => {
+  req.session.destroy(() => {
     res.redirect('/');
-  })
-}
+  });
+};
+
+
+const getDashboardPage = async(req, res) => {
+  const user = await User.findOne({_id:req.session.userID});
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user
+  });
+};
+
 
 module.exports = {
   createUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  getDashboardPage
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post('/signup', userController.createUser); //   localhost:3000/users/signup
 router.post('/login', userController.loginUser); //   localhost:3000/users/login
 router.get('/logout', userController.logoutUser); //   localhost:3000/users/logout
+router.get('/dashboard', userController.getDashboardPage); //   localhost:3000/users/dashboard
 
 
 module.exports ={
