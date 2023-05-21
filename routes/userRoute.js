5e6b778d7c6e07acc3a067a8bd18b721e9d6c218
brknt <brknt.gns@hotmail.com>
2023-05-22 02:27:51 +0300

    added middlewares

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const userController = require('../controllers/authController');
+const authMiddleware = require('../middlewares/authMiddleware');
+
 
 const router = express.Router();
 
-router.post('/signup', userController.createUser); //   localhost:3000/users/signup
+router.post('/signup',  userController.createUser); //   localhost:3000/users/signup
 router.post('/login', userController.loginUser); //   localhost:3000/users/login
 router.get('/logout', userController.logoutUser); //   localhost:3000/users/logout
-router.get('/dashboard', userController.getDashboardPage); //   localhost:3000/users/dashboard
+router.get('/dashboard', authMiddleware, userController.getDashboardPage); //   localhost:3000/users/dashboard
 
 
 module.exports ={
