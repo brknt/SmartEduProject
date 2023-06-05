5faa061af14f3b01a4ccc9146c234ef55900f043
brknt <brknt.gns@hotmail.com>
2023-06-05 16:37:13 +0300

    added category create and delete

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -3,10 +3,22 @@ const Category = require('../models/Category');
 const createCategory = async (req, res) => {
   try {
     const category = await Category.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      category,
+    res.status(201).redirect('/users/dashboard');
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
     });
+  }
+};
+
+
+const deleteCategory = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    await Category.findByIdAndRemove(req.params.id);
+    // req.flash('error', `${course.name} has been removed successfuly`);
+    res.status(200).redirect('/users/dashboard');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -18,5 +30,6 @@ const createCategory = async (req, res) => {
 
 
 module.exports = {
-    createCategory,    
+    createCategory,
+    deleteCategory    
 }
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,6 +4,8 @@ const categoryController = require('../controllers/categoryController');
 const router = express.Router();
 
 router.post('/', categoryController.createCategory);
+router.delete('/:id',  categoryController.deleteCategory); //   localhost:3000/users/dashboard
+
 
 module.exports ={
     routes:router
